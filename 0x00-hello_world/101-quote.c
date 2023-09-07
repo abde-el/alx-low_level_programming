42db20e2be2a129ce5d931b92c2ca97f11899d2a
abde-el <abdewakilmoukdad@gmail.com>
2023-09-07 21:45:33 +0200

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: 1 (not success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
