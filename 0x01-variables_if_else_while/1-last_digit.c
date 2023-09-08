73c0769042cc625f1c7c73975e01fe5b7f2ffec1
abde-el <abdewakilmoukdad@gmail.com>
2023-09-08 16:53:49 +0200

    a

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descreption : A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+
+	if (digit > 5)
+	printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+	printf("Last digit of %i is %i and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+	return (0);
+}
