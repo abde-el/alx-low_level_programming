f74f18ed1e202d77b2c368ed7cd50719234bef4e
abde-el <abdewakilmoukdad@gmail.com>
2023-09-12 21:43:07 +0200

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Entry point
+ * print_alphabet - Entry point
  * Return:0
 */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	int i;
-	int j;
 
-	for (j = 1; i <= 10; i++)
+	for (j = 97; i <= 122; i++)
 	{
-		for (j = 97; j <= 122; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
+		_putchar(i);
 	}
+	_putchar('\n');
 
 }
